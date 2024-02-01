2a6176fc525cb686da5e1469fc94cc65778c91d1
Leandro Boari <leandrobnsilva@gmail.com>
2024-02-01 16:11:45 -0300

    Primeiro upload

---
+++ template/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Leandro Boari</title>
+<link href="./assets/css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
